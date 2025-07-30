61b7d42ee4045127b5a13743c3b7a4edfb9d5e56
Vasiliy Nikiforov <nikiforov_vk@drom.ru>
2025-07-30 11:24:09 +1000

    Создать отдельную функцию инициализации логера

---
+++ src/infrastructure/logger_init.py
@@ -0,0 +1,27 @@
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+def logger_init():
+    # TODO Инициализацию логера и перехват исключений убрать в отдельные классы
+    formatter = logging.Formatter('%(asctime)s : %(levelname)s - %(message)s')
+
+    file_handler = TimedRotatingFileHandler(
+        'bot.log',
+        when='midnight',
+        interval=1,
+        backupCount=0,
+    )
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+
+    result_logger = logging.getLogger('bot')
+    result_logger.setLevel(logging.INFO)
+    result_logger.addHandler(file_handler)
+    result_logger.addHandler(console_handler)
+
+    return result_logger
