810e46ac80a7bb051d36c63f5be135781e0b83f9
Vasiliy Nikiforov <nikiforov_vk@drom.ru>
2025-07-30 11:26:12 +1000

    Добавить новые команды в для бота

---
+++ src/presenters/commands/telegram.py
@@ -0,0 +1,58 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+
+from src.applications.events import create
+from src.applications.events.get_list import GetEventList
+from src.infrastructure.logger_init import logger_init
+from src.repositories import EventRepository, EventMemberRepository
+
+logger = logger_init()
+
+async def event_create(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.message.chat_id
+    member_id = update.message.from_user.id
+    user_info = await context.bot.get_chat_member(chat_id, member_id)
+    user_name = user_info.user.full_name
+    nick_name = user_info.user.username
+    text = update.message.text
+
+    event_create_executor = create.CreateEvent(
+        event_repository=EventRepository(),
+        event_member_repository=EventMemberRepository()
+    )
+
+    params = create.CreateEvenParams(
+        chat_id=chat_id,
+        member_id=member_id,
+        text=text,
+        nick_name=nick_name,
+        user_name=user_name,
+    )
+
+    message = event_create_executor.execute(params)
+
+    try:
+        await context.bot.send_message(chat_id=chat_id, text=message)
+    except:
+        logger.error(f"Failed send message {message}")
+
+    pass
+
+async def events(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.message.chat_id
+    member_id = update.message.from_user.id
+
+    getter = GetEventList(
+        repository=EventRepository()
+    )
+    messages = getter.execute(
+        chat_id=chat_id,
+        member_id=member_id,
+    )
+
+    message = '\n'.join(messages)
+
+    try:
+        await context.bot.send_message(chat_id=chat_id, text=message)
+    except:
+        logger.error(f"Failed send message {message}")
