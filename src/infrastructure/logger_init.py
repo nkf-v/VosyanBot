96a8dafe3dc06b457b98829404643a28c68d938e
Vasiliy Nikiforov <nikiforov_vk@drom.ru>
2025-08-06 11:15:36 +1000

    Изменить конфигурацию логирования запросов в телеграм

--- src/infrastructure/logger_init.py
+++ src/infrastructure/logger_init.py
@@ -22,12 +22,12 @@ def logger_init():
     tg_logger = logging.getLogger("telegram")
     tg_logger.addHandler(file_handler)
     tg_logger.addHandler(console_handler)
-    tg_logger.setLevel(logging.INFO)
+    tg_logger.setLevel(logging.WARNING)
 
     httpx_logger = logging.getLogger("httpx")
     httpx_logger.addHandler(file_handler)
     httpx_logger.addHandler(console_handler)
-    httpx_logger.setLevel(logging.INFO)
+    httpx_logger.setLevel(logging.WARNING)
 
     peewee_logger = logging.getLogger("peewee")
     peewee_logger.addHandler(file_handler)
